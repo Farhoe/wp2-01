b5ae656328a512d99f342bfc95b3e759187d7324
Radar4861 <matej.dalekorej@student.ossp.cz>
2018-10-02 11:24:11 +0100

    Zkoro funkčňý

--- index.php
+++ index.php
@@ -5,9 +5,24 @@
     <title></title>
   </head>
   <body>
+    <h1>Vybírání autíčka</h1>
+
   <form class="" action="submit.php" method="post">
   <label for="spd">SPD</label>
-  <input type="spd" name="text" value="spd">
+  <input id="spd" type="text" name="spd" placeholder="SPD je moje oblíbená politická strana">
+<br />
+  <label for="auta">Auto</label>
+  <select for="auta" name="auta">
+    <option value="hitlerovo-auto">Hitlerovo auto</option>
+    <option value="ford">Ford</option>
+    <option value="mini">Mini</option>
+    <option value="fiat">Fiat</option>
+    <option value="jeep"></option>
+    <option value="skoda">Škoda</option>
+  </select>
+ <br />
+
+  <input type="submit" name="submit" value="Posílám ti to na tady máš">
   </form>
   </body>
 </html>
