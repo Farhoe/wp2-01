5b6a2eed167673dcfdd3127eab17fbb47eb77b45
Radar4861 <matej.dalekorej@student.ossp.cz>
2018-10-02 11:05:32 +0100

    Ahoj kluku

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <form class="" action="submit.php" method="post">
+  <label for="spd">SPD</label>
+  <input type="spd" name="text" value="spd">
+  </form>
+  </body>
+</html>
